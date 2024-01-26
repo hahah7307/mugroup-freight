b03091350539b39b285d27f4e680a92d9de65225
hahah7307 <1084429753@qq.com>
2024-01-26 09:37:36 +0800

    运费计算1-73（每日结算库存逻辑和时间优化）

--- application/Manage/command/DateStockUpdate.php
+++ application/Manage/command/DateStockUpdate.php
@@ -34,48 +34,50 @@ class DateStockUpdate extends Command
         Config::load(APP_PATH . 'Manage/config.php');
         Config::load(APP_PATH . 'storage.php');
 
-        $warehouseIds = getWarehouseID();
         $ymd = date('Ymd');
         $receivingItemObj = new ReceivingItemModel();
 
-        $dataStockReceiving1 = DateStockUpdateModel::get(1);
-        if ($dataStockReceiving1['date'] < $ymd) {
-            $sql = 'SELECT SUM( i.rd_received_qty ) quantity_sum, i.product_barcode product_sku, r.warehouse_id warehouse_id, DATE_FORMAT(now(), "%Y%m%d") date FROM mu_ecang_receiving r RIGHT JOIN mu_ecang_receiving_item i ON r.id = i.receiving_id WHERE i.rd_status > 0 AND r.complete_time > "' . Config::get('stock_date') . '" AND r.warehouse_id IN (' . implode(',', $warehouseIds) . ') GROUP BY i.product_barcode, r.warehouse_id;';
-            $resData = $receivingItemObj->query($sql);
-            $receivingDate = new DateStockReceivingModel();
-            $receivingDate->insertAll($resData);
-            $stockOpening = StockOpeningModel::all();
-            $updateData = [];
-            $insertData = [];
-            foreach ($stockOpening as $item) {
-                $receivingItem = $receivingDate->where(['product_sku' => $item['product_sku'], 'warehouse_id' => $item['warehouse_id'], 'date' => $ymd])->find();
-                if ($receivingItem) {
-                    $receivingItem['quantity_sum'] = $receivingItem['quantity_sum'] + $item['stock'];
-                    $updateData[] = $receivingItem->toArray();
-                } else {
-                    $receivingItem = [
-                        'quantity_sum'  =>  $item['stock'],
-                        'product_sku'   =>  $item['product_sku'],
-                        'warehouse_id'  =>  $item['warehouse_id'],
-                        'date'          =>  $ymd
-                    ];
-                    $insertData[] = $receivingItem;
+        $dataStockUpdate = DateStockUpdateModel::all();
+        foreach ($dataStockUpdate as $update) {
+            if ($update['date'] < $ymd && $update['hour'] <= date('H')) {
+                $warehouseIds = explode(',', $update['warehouseIds']);
+                if ($update['model'] == "DateStockReceivingModel") {
+                    $sql = 'SELECT SUM( i.rd_received_qty ) quantity_sum, i.product_barcode product_sku, r.warehouse_id warehouse_id, DATE_FORMAT(now(), "%Y%m%d") date FROM mu_ecang_receiving r RIGHT JOIN mu_ecang_receiving_item i ON r.id = i.receiving_id WHERE i.rd_status > 0 AND r.receiving_type = 0 AND r.complete_time > "' . Config::get('stock_date') . '" AND r.warehouse_id IN (' . $update['warehouseIds'] . ') GROUP BY i.product_barcode, r.warehouse_id;';
+                    $resData = $receivingItemObj->query($sql);
+                    $receivingDate = new DateStockReceivingModel();
+                    $receivingDate->insertAll($resData);
+                    $stockOpeningObj = new StockOpeningModel();
+                    $stockOpening = $stockOpeningObj->where(['warehouse_id' => ['in', $warehouseIds]])->select();
+                    $updateData = [];
+                    $insertData = [];
+                    foreach ($stockOpening as $item) {
+                        $receivingItem = $receivingDate->where(['product_sku' => $item['product_sku'], 'warehouse_id' => $item['warehouse_id'], 'date' => $ymd])->find();
+                        if ($receivingItem) {
+                            $receivingItem['quantity_sum'] = $receivingItem['quantity_sum'] + $item['stock'];
+                            $updateData[] = $receivingItem->toArray();
+                        } else {
+                            $receivingItem = [
+                                'quantity_sum'  =>  $item['stock'],
+                                'product_sku'   =>  $item['product_sku'],
+                                'warehouse_id'  =>  $item['warehouse_id'],
+                                'date'          =>  $ymd
+                            ];
+                            $insertData[] = $receivingItem;
+                        }
+                    }
+                    $receivingDate->saveAll($updateData);
+                    $receivingDate->insertAll($insertData);
+
+                    DateStockUpdateModel::update(['date' => $ymd], ['id' => $update['id']]);
+                } elseif ($update['model'] == "DateStockConsumeModel") {
+                    $sql = 'SELECT SUM( d.qty ) quantity_sum, d.warehouseSku product_sku, o.warehouseId warehouse_id, DATE_FORMAT( now( ), "%Y%m%d" ) date FROM mu_ecang_order_detail d LEFT JOIN mu_ecang_order o ON o.id = d.order_id WHERE o.STATUS = 4 AND o.dateWarehouseShipping > "' . Config::get('stock_date') . '" AND o.warehouseId IN (' . $update['warehouseIds'] . ') GROUP BY d.warehouseSku, o.warehouseId;';
+                    $resData = $receivingItemObj->query($sql);
+                    $consumeDate = new DateStockConsumeModel();
+                    $consumeDate->insertAll($resData);
+
+                    DateStockUpdateModel::update(['date' => $ymd], ['id' => $update['id']]);
                 }
             }
-            $receivingDate->saveAll($updateData);
-            $receivingDate->insertAll($insertData);
-
-            DateStockUpdateModel::update(['date' => $ymd], ['id' => 1]);
-        }
-
-        $dataStockReceiving2 = DateStockUpdateModel::get(2);
-        if ($dataStockReceiving2['date'] < $ymd) {
-            $sql2 = 'SELECT SUM( d.qty ) quantity_sum, d.productSku product_sku, o.warehouseId warehouse_id, DATE_FORMAT( now( ), "%Y%m%d" ) date FROM mu_ecang_order_detail d LEFT JOIN mu_ecang_order o ON o.id = d.order_id WHERE o.STATUS = 4 AND o.dateWarehouseShipping > "' . Config::get('stock_date') . '" AND o.warehouseId IN (' . implode(',', $warehouseIds) . ') GROUP BY d.productSku, o.warehouseId;';
-            $resData2 = $receivingItemObj->query($sql2);
-            $consumeDate = new DateStockConsumeModel();
-            $consumeDate->insertAll($resData2);
-
-            DateStockUpdateModel::update(['date' => $ymd], ['id' => 2]);
         }
 
         $output->writeln("success");
