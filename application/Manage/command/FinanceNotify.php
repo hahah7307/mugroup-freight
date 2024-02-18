3ea7a12a7e44f220ef52e0b207f46bf3e94a23f6
hahah7307 <1084429753@qq.com>
2024-02-18 15:27:40 +0800

    运费计算1-81（财报关联出库数据，新增仓库sku，采购和运营）

--- application/Manage/command/FinanceNotify.php
+++ application/Manage/command/FinanceNotify.php
@@ -3,6 +3,7 @@ namespace app\Manage\command;
 
 use app\Manage\model\FinanceOrderModel;
 use app\Manage\model\FinanceOrderOutboundModel;
+use app\Manage\model\OrderDetailModel;
 use app\Manage\model\OrderModel;
 use Exception;
 use think\Config;
@@ -32,17 +33,27 @@ class FinanceNotify extends Command
             $list = $financeObj->where(['is_notify' => 0])->order('id asc')->limit(Config::get('finance_notify_num'))->select();
             $newData = [];
             foreach ($list as $item) {
-                $finance = OrderModel::all(['refNo' => $item['payment_id'], 'status' => 4]);
+                $finance = OrderModel::all(['refNo' => $item['payment_id']]);
                 $outbound_id = [];
                 foreach ($finance as $outbound) {
+                    $orderDetailObj = new OrderDetailModel();
+                    $orderDetail = $orderDetailObj->with('product')->where(['order_id' => $outbound['id']])->find()->toArray();
                     $outbound_id[] = $outbound['id'];
                     $newData[] = [
+                        'table_id'          =>  $item['table_id'],
+                        'finance_order_id'  =>  $item['id'],
                         'ecang_order_id'    =>  $outbound['id'],
                         'payment_id'        =>  $item['payment_id'],
                         'saleOrderCode'     =>  $outbound['saleOrderCode'],
+                        'warehouse_sku'     =>  $orderDetail['product']['productSku'],
+                        'oprater_id'        =>  $orderDetail['product']['personOpraterId'],
+                        'seller_id'         =>  $orderDetail['product']['personSellerId'],
                         'currency'          =>  $outbound['currency'],
-                        'amountpaid'        =>  $outbound['amountpaid'],
-                        'created_date'      =>  date('Y-m-d')
+                        'paid_amount'       =>  $outbound['amountpaid'],
+                        'tail_amount'       =>  $outbound['calcuRes'],
+                        'warehouse_id'      =>  $outbound['warehouseId'],
+                        'warehouse_code'    =>  $outbound['warehouseCode'],
+                        'created_date'      =>  date('Y-m-d H:i:s')
                     ];
                 }
                 $notifyData = [
