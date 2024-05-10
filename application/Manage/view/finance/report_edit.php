772c0bcf2ac7de8618e99ea6debdcd013f39cf79
hahah7307 <1084429753@qq.com>
2024-05-10 09:43:53 +0800

    运费计算1-151（财报维度新增三币种汇率)

--- application/Manage/view/finance/report_edit.php
+++ application/Manage/view/finance/report_edit.php
@@ -19,6 +19,24 @@
                     <input type="text" class="layui-input" id="month" name="month" value="{$info.month}" placeholder="请选择月份">
                 </div>
             </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label">美元汇率</label>
+                <div class="layui-input-inline w300">
+                    <input type="text" class="layui-input" name="USD" value="{$info.USD}" placeholder="请选择美元汇率">
+                </div>
+            </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label">欧元汇率</label>
+                <div class="layui-input-inline w300">
+                    <input type="text" class="layui-input" name="EUR" value="{$info.EUR}" placeholder="请选择欧元汇率">
+                </div>
+            </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label">澳元汇率</label>
+                <div class="layui-input-inline w300">
+                    <input type="text" class="layui-input" name="AUD" value="{$info.AUD}" placeholder="请选择澳元汇率">
+                </div>
+            </div>
 			<div class="layui-form-item">
 				<div class="layui-input-block">
 					<button class="layui-btn w200" lay-submit lay-filter="formCoding">提交保存</button>
