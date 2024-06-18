8cd39b1366793339e094661a105ca85c7132ae6c
hahah7307 <1084429753@qq.com>
2024-06-18 09:36:10 +0800

    运费计算1-208（产品更新逻辑优化)

--- application/Manage/command/ProductUpdate.php
+++ application/Manage/command/ProductUpdate.php
@@ -36,6 +36,8 @@ class ProductUpdate extends Command
             && $ecUpdate['is_finished'] == 1
             && date('Ymd') == $lcUpdate['date']
             && $lcUpdate['is_finished'] == 1
+            && date('Ymd') == $leUpdate['date']
+            && $leUpdate['is_finished'] == 1
         ) {
             echo "success";exit();
         }
@@ -60,79 +62,91 @@ class ProductUpdate extends Command
         Db::startTrans();
         try {
             // 易仓产品更新
-            $ecProductRes = ApiClient::EcWarehouseApi(Config::get("ec_wms_uri"), "getProductList", '{"page":' . $ecUpdate['page'] . '}');
-            $ecProductList = $ecProductRes['data'];
-            if (count($ecProductList) <= 0) {
-                ProductUpdateModel::update(['id' => $ecUpdate['id'], 'page' => $ecUpdate['page'] + 1, 'is_finished' => 1]);
+            if (date('Ymd') == $ecUpdate['date'] && $ecUpdate['is_finished'] == 1){
+                echo "success";
             } else {
-                $addData = [];
-                foreach ($ecProductList as $item) {
-                    $productInfo = ProductModel::get(['productSku' => $item['productSku']]);
-                    if (!empty($productInfo)) {
-                        continue;
+                $ecProductRes = ApiClient::EcWarehouseApi(Config::get("ec_wms_uri"), "getProductList", '{"page":' . $ecUpdate['page'] . '}');
+                $ecProductList = $ecProductRes['data'];
+                if (count($ecProductList) <= 0) {
+                    ProductUpdateModel::update(['id' => $ecUpdate['id'], 'is_finished' => 1]);
+                } else {
+                    $addData = [];
+                    foreach ($ecProductList as $item) {
+                        $productInfo = ProductModel::get(['productSku' => $item['productSku']]);
+                        if (!empty($productInfo)) {
+                            continue;
+                        }
+                        $productDetail = $item;
+                        unset($productDetail['productPackage']);
+                        unset($productDetail['productCost']);
+                        $productDetail['productPackage'] = json_encode($item['productPackage']);
+                        $productDetail['productCost'] = json_encode($item['productCost']);
+                        $addData[] = $productDetail;
+                        unset($item);
                     }
-                    $productDetail = $item;
-                    unset($productDetail['productPackage']);
-                    unset($productDetail['productCost']);
-                    $productDetail['productPackage'] = json_encode($item['productPackage']);
-                    $productDetail['productCost'] = json_encode($item['productCost']);
-                    $addData[] = $productDetail;
-                    unset($item);
+                    unset($ecProductList);
+                    $productObj = new ProductModel();
+                    $productObj->saveAll($addData);
+                    ProductUpdateModel::update(['id' => $ecUpdate['id'], 'page' => $ecUpdate['page'] + 1]);
+                    unset($addData);
                 }
-                unset($ecProductList);
-                $productObj = new ProductModel();
-                $productObj->saveAll($addData);
-                ProductUpdateModel::update(['id' => $ecUpdate['id'], 'page' => $ecUpdate['page'] + 1]);
-                unset($addData);
             }
 
             // 良仓产品更新
-            $lcProductRes = ApiClient::LcWarehouseApi("getProductList", '{"pageSize":50,"page":' . $lcUpdate['page'] . '}');
-            $lcProductList = $lcProductRes['data'];
-            if (count($lcProductList) <= 0) {
-                ProductUpdateModel::update(['id' => $lcUpdate['id'], 'page' => $lcUpdate['page'] + 1, 'is_finished' => 1]);
+            if (date('Ymd') == $lcUpdate['date'] && $lcUpdate['is_finished'] == 1){
+                echo "success";
             } else {
-                $addData = [];
-                foreach ($lcProductList as $item) {
-                    $productInfo = LcProductModel::get(['product_sku' => $item['product_sku']]);
-                    if (!empty($productInfo)) {
-                        continue;
+                $lcProductRes = ApiClient::LcWarehouseApi("getProductList", '{"pageSize":50,"page":' . $lcUpdate['page'] . '}');
+                $lcProductList = $lcProductRes['data'];
+                if (count($lcProductList) <= 0) {
+                    ProductUpdateModel::update(['id' => $lcUpdate['id'], 'is_finished' => 1]);
+                } else {
+                    $addData = [];
+                    foreach ($lcProductList as $item) {
+                        $productInfo = LcProductModel::get(['product_sku' => $item['product_sku']]);
+                        if (!empty($productInfo)) {
+                            continue;
+                        }
+                        $productDetail = $item;
+                        unset($productDetail['warehouse_attribute']);
+                        $productDetail['warehouse_attribute'] = json_encode($item['warehouse_attribute']);
+                        $addData[] = $productDetail;
+                        unset($item);
                     }
-                    $productDetail = $item;
-                    unset($productDetail['warehouse_attribute']);
-                    $productDetail['warehouse_attribute'] = json_encode($item['warehouse_attribute']);
-                    $addData[] = $productDetail;
-                    unset($item);
+                    unset($lcProductList);
+                    $productObj = new LcProductModel();
+                    $productObj->saveAll($addData);
+                    ProductUpdateModel::update(['id' => $lcUpdate['id'], 'page' => $lcUpdate['page'] + 1]);
+                    unset($addData);
                 }
-                unset($lcProductList);
-                $productObj = new LcProductModel();
-                $productObj->saveAll($addData);
-                ProductUpdateModel::update(['id' => $lcUpdate['id'], 'page' => $lcUpdate['page'] + 1]);
-                unset($addData);
             }
 
             // 乐歌产品更新
-            $leProductParams = ['pageNum' => $leUpdate['page'], 'pageSize' => 50];
-            $leProductRes = ApiClient::LeWarehouseApi("https://app.lecangs.com/api/oms/goods/api/list", "POST", $leProductParams);
-            $leProductList = $leProductRes['data']['list'];
-            if (count($leProductList) <= 0) {
-                ProductUpdateModel::update(['id' => $leUpdate['id'], 'page' => $leUpdate['page'] + 1, 'is_finished' => 1]);
+            if (date('Ymd') == $leUpdate['date'] && $leUpdate['is_finished'] == 1){
+                echo "success";
             } else {
-                $addData = [];
-                foreach ($leProductList as $item) {
-                    $productInfo = LeProductModel::get(['code' => $item['code']]);
-                    if (!empty($productInfo)) {
-                        continue;
+                $leProductParams = ['pageNum' => $leUpdate['page'], 'pageSize' => 50];
+                $leProductRes = ApiClient::LeWarehouseApi("https://app.lecangs.com/api/oms/goods/api/list", "POST", $leProductParams);
+                $leProductList = $leProductRes['data']['list'];
+                if (count($leProductList) <= 0) {
+                    ProductUpdateModel::update(['id' => $leUpdate['id'], 'is_finished' => 1]);
+                } else {
+                    $addData = [];
+                    foreach ($leProductList as $item) {
+                        $productInfo = LeProductModel::get(['code' => $item['code']]);
+                        if (!empty($productInfo)) {
+                            continue;
+                        }
+                        $productDetail = $item;
+                        $addData[] = $productDetail;
+                        unset($item);
                     }
-                    $productDetail = $item;
-                    $addData[] = $productDetail;
-                    unset($item);
+                    unset($leProductList);
+                    $productObj = new LeProductModel();
+                    $productObj->saveAll($addData);
+                    ProductUpdateModel::update(['id' => $leUpdate['id'], 'page' => $leUpdate['page'] + 1]);
+                    unset($addData);
                 }
-                unset($leProductList);
-                $productObj = new LeProductModel();
-                $productObj->saveAll($addData);
-                ProductUpdateModel::update(['id' => $leUpdate['id'], 'page' => $leUpdate['page'] + 1]);
-                unset($addData);
             }
 
             Db::commit();
