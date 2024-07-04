3d0963d00c384a772bf7ef50c3ce490fbb86bb0d
hahah7307 <1084429753@qq.com>
2024-07-04 18:37:22 +0800

    运费计算1-229（财报促销费用新增促销关键词)

--- application/Manage/controller/FinanceController.php
+++ application/Manage/controller/FinanceController.php
@@ -734,7 +734,7 @@ class FinanceController extends BaseController
                     if (!$financeOrderPromotionObj->saveAll($paymentData['orderPromotionNew'])) {
                         throw new \think\Exception('Payment导入失败！');
                     } else {
-                        $promotionSum = $financeOrderPromotionObj->where(['table_id' => $tableId, 'description' => [['like', '%Coupon Redemption Fee%'], ['like', '%Vine Enrollment Fee%'], ['like', '%秒杀%'], 'or']])->sum('total');
+                        $promotionSum = $financeOrderPromotionObj->where(['table_id' => $tableId, 'description' => [['like', '%Coupon Redemption Fee%'], ['like', '%Vine Enrollment Fee%'], ['like', '%秒杀%'], ['like', '%促销%'], 'or']])->sum('total');
                     }
 
                     $financeOrderShippingServiceObj = new FinanceOrderShippingServiceModel();
