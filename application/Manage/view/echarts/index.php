db4d846fcc68b9f40911825c1d692ab2b9909099
hahah7307 <1084429753@qq.com>
2024-07-24 18:05:28 +0800

    运费计算1-270（美国各州销量热力图)

--- application/Manage/view/echarts/index.php
+++ application/Manage/view/echarts/index.php
@@ -1,21 +1,94 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8"/>
-    <title>USA Map</title>
-</head>
+
+{include file="public/header" /}
+<style>
+    .right {min-width: 480px; width: 480px}
+    #USAMap {z-index: 99999999}
+</style>
 <script src="/static/echarts/dist/echarts.min.js"></script>
 <script src="/static/echarts/test/lib/jquery.min.js"></script>
-<body>
-<div id="USAMap" style="width: 1800px;height:1200px;"></div>
-</body>
+<div class="layui-body" id="LAY_app_body">
+    <div class="right" style="width: 480px">
+        <div class="title">美国各州销量热力图</div>
+        <form class="layui-form" method="get">
+            <div class="layui-form-item">
+                <label class="layui-form-label">开始时间</label>
+                <div class="layui-input-inline w300">
+                    <input type="text" class="layui-input" id="sale_start" name="sale_start" value="{$sale_start}" placeholder="开始时间">
+                </div>
+            </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label">结束时间</label>
+                <div class="layui-input-inline w300">
+                    <input type="text" class="layui-input" id="sale_end" name="sale_end" value="{$sale_end}" placeholder="结束时间">
+                </div>
+            </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label">参考数量</label>
+                <div class="layui-input-inline w300">
+                    <input type="text" class="layui-input" name="quantity" value="{$quantity}" placeholder="请填写参考数量">
+                </div>
+            </div>
+            {foreach name="sku" item="v" key="k"}
+            <div class="layui-form-item">
+                <label class="layui-form-label">{if condition="$k eq 0"}仓库SKU{/if}</label>
+                <div class="layui-input-inline w300">
+                    <input type="text" class="layui-input" name="sku[]" value="{$v}" placeholder="请填写仓库SKU">
+                </div>
+                {if condition="$k eq 0"}
+                <button class="layui-btn layui-btn-sm btn-lc" lay-submit lay-filter="AttrAdd">添加</button>
+                {else/}
+                <button class="layui-btn layui-btn-sm layui-btn-danger btn-lc" lay-submit lay-filter="attrDel">删除</button>
+                {/if}
+            </div>
+            {/foreach}
+            <div class="layui-form-item" id="sub-dom">
+                <div class="layui-input-block">
+                    <button class="layui-btn w200" lay-submit lay-filter="Search"><i class="layui-icon">&#xe615;</i> 查询</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<div id="USAMap" style="width: 1800px;height:1200px;margin-left: 720px"></div>
 
+<script>
+    layui.use(['form', 'jquery', 'laydate'], function(){
+        let $ = layui.jquery,
+            form = layui.form,
+            laydate = layui.laydate;
+
+        // 显示日期选择器
+        laydate.render({
+            elem: '#sale_start',
+            type: 'date'
+        });
+        laydate.render({
+            elem: '#sale_end',
+            type: 'date'
+        });
+
+        let domIndex = 0;
+        // 添加属性
+        form.on('submit(AttrAdd)', function(data) {
+            domIndex ++;
+            let newDom = '<div class="layui-form-item"><label class="layui-form-label"></label><div class="layui-input-inline w300"><input type="text" class="layui-input" name="sku[' + domIndex + ']" placeholder="请填写仓库SKU"></div><button class="layui-btn layui-btn-sm layui-btn-danger btn-lc" lay-submit lay-filter="attrDel">删除</button></div>';
+            $("#sub-dom").before(newDom);
+            form.render();
+            return false;
+        });
+
+        // 删除属性
+        form.on('submit(attrDel)', function(data) {
+            $(this).parent().remove();
+        });
+    });
+</script>
 <script>
     // 美国地图
     USAMap();
 
     function USAMap() {
-        var myChart = echarts.init(document.getElementById("USAMap"));
+        let myChart = echarts.init(document.getElementById("USAMap"));
         // 开启加载loading的动画
         myChart.showLoading();
         // jquery读取json文件
@@ -66,14 +139,14 @@
                 // 可视映射
                 visualMap: {
                     left: 'right',
-                    min: 500000,
-                    max: 38000000,
+                    min: 0,
+                    max: {$quantity},
                     // 颜色区间
                     inRange: {
                         color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
                     },
                     // 文本，默认为数值文本
-                    text: ['High', 'Low'],
+                    text: ['最高销售量', '最低销售量'],
                     // 显示拖拽用的手柄
                     calculable: true
                 },
@@ -94,7 +167,7 @@
                 },
                 series: [
                     {
-                        name: 'USA PopEstimates',
+                        name: '销售数量',
                         type: 'map',
                         // 开启鼠标缩放和平移漫游
                         roam: true,
@@ -120,60 +193,7 @@
                                 }
                             }
                         },
-                        data: [
-                            {name: 'Alabama', value: 4822023},
-                            {name: 'Alaska', value: 731449},
-                            {name: 'Arizona', value: 6553255},
-                            {name: 'Arkansas', value: 2949131},
-                            {name: 'California', value: 38041430},
-                            {name: 'Colorado', value: 5187582},
-                            {name: 'Connecticut', value: 3590347},
-                            {name: 'Delaware', value: 917092},
-                            {name: 'District of Columbia', value: 632323},
-                            {name: 'Florida', value: 19317568},
-                            {name: 'Georgia', value: 9919945},
-                            {name: 'Hawaii', value: 1392313},
-                            {name: 'Idaho', value: 1595728},
-                            {name: 'Illinois', value: 12875255},
-                            {name: 'Indiana', value: 6537334},
-                            {name: 'Iowa', value: 3074186},
-                            {name: 'Kansas', value: 2885905},
-                            {name: 'Kentucky', value: 4380415},
-                            {name: 'Louisiana', value: 4601893},
-                            {name: 'Maine', value: 1329192},
-                            {name: 'Maryland', value: 5884563},
-                            {name: 'Massachusetts', value: 6646144},
-                            {name: 'Michigan', value: 9883360},
-                            {name: 'Minnesota', value: 5379139},
-                            {name: 'Mississippi', value: 2984926},
-                            {name: 'Missouri', value: 6021988},
-                            {name: 'Montana', value: 1005141},
-                            {name: 'Nebraska', value: 1855525},
-                            {name: 'Nevada', value: 2758931},
-                            {name: 'New Hampshire', value: 1320718},
-                            {name: 'New Jersey', value: 8864590},
-                            {name: 'New Mexico', value: 2085538},
-                            {name: 'New York', value: 19570261},
-                            {name: 'North Carolina', value: 9752073},
-                            {name: 'North Dakota', value: 699628},
-                            {name: 'Ohio', value: 11544225},
-                            {name: 'Oklahoma', value: 3814820},
-                            {name: 'Oregon', value: 3899353},
-                            {name: 'Pennsylvania', value: 12763536},
-                            {name: 'Rhode Island', value: 1050292},
-                            {name: 'South Carolina', value: 4723723},
-                            {name: 'South Dakota', value: 833354},
-                            {name: 'Tennessee', value: 6456243},
-                            {name: 'Texas', value: 26059203},
-                            {name: 'Utah', value: 2855287},
-                            {name: 'Vermont', value: 626011},
-                            {name: 'Virginia', value: 8185867},
-                            {name: 'Washington', value: 6897012},
-                            {name: 'West Virginia', value: 1855413},
-                            {name: 'Wisconsin', value: 5726398},
-                            {name: 'Wyoming', value: 576412},
-                            {name: 'Puerto Rico', value: 3667084}
-                        ]
+                        data: {$list}
                     }
                 ]
             };
@@ -185,4 +205,4 @@
 
 </script>
 
-</html>
+{include file="public/footer" /}
