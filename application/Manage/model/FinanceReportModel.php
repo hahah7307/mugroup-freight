7a4b6ffc59428275b525f56bd18917fdd9ceeec1
hahah7307 <1084429753@qq.com>
2024-08-15 17:04:26 +0800

    运费计算1-297（财报Walmart导出表新增台账费用)

--- application/Manage/model/FinanceReportModel.php
+++ application/Manage/model/FinanceReportModel.php
@@ -2124,38 +2124,42 @@ SELECT
 	SUM( ROUND( sale_selling_fees, 7 ) ) sale_selling_fees,
 	SUM( ROUND( refund_selling_fees, 7 ) ) refund_selling_fees,
 	SUM( ROUND( calcuRes, 7 ) ) calcuRes,
-	SUM( ROUND( wfs_tail, 6 ) ) wfs_tail,
+	SUM( ROUND( wfs_fulfillment, 6 ) ) wfs_fulfillment,
 	SUM( ROUND( ddp, 2 ) ) ddp,
 	SUM( ROUND( adCost, 7 ) ) adCost,
 	SUM( ROUND( warehouse_rent, 4 ) ) warehouse_rent,
 	SUM( ROUND( wfs_warehouse, 8 ) ) wfs_warehouse,
+	SUM( ROUND( wfs_return_shipping, 6 ) ) wfs_return_shipping,
 	SUM( ROUND( adjustment, 6 ) ) adjustment,
 	SUM( ROUND( lc_adjustment, 6 ) ) lc_adjustment,
 	SUM( ROUND( le_adjustment, 6 ) ) le_adjustment,
 	SUM( ROUND( wfs_adjustment, 6 ) ) wfs_adjustment,
+	SUM( ROUND( operation_expenses, 6 ) ) operation_expenses,
+	SUM( ROUND( operation_factory, 6 ) ) operation_factory,
+	SUM( ROUND( operation_delivery, 6 ) ) operation_delivery,
 	ROUND( SUM( IFNULL( adCost, 0 ) ) / SUM( IFNULL( sale_amount, 0 ) ) * - 1, 4 ) ad_percent,
 	ROUND( SUM( IFNULL( warehouse_rent, 0 ) ) / SUM( IFNULL( sale_amount, 0 ) ) * - 1, 4 ) warehouse_percent,
 	ROUND( SUM( IFNULL( calcuRes, 0 ) ) / SUM( IFNULL( sale_amount, 0 ) ) * - 1, 4 ) tail_percent,
 	ROUND( SUM( IFNULL( ddp, 0 ) ) / SUM( IFNULL( sale_amount, 0 ) ) * - 1, 4 ) ddp_percent,
 	ROUND(
-		SUM( IFNULL( sale_amount, 0 ) ) + SUM( IFNULL( refund_amount, 0 ) ) + SUM( IFNULL( sale_selling_fees, 0 ) ) + SUM( IFNULL( refund_selling_fees, 0 ) ) + SUM( IFNULL( calcuRes, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( ddp, 0 ) ) + SUM( IFNULL( adCost, 0 ) ) + SUM( IFNULL( warehouse_rent, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( adjustment, 0 ) ) + SUM( IFNULL( lc_adjustment, 0 ) ) + SUM( IFNULL( le_adjustment, 0 ) ) + SUM( IFNULL( wfs_adjustment, 0 ) ),
+		SUM( IFNULL( sale_amount, 0 ) ) + SUM( IFNULL( refund_amount, 0 ) ) + SUM( IFNULL( sale_selling_fees, 0 ) ) + SUM( IFNULL( refund_selling_fees, 0 ) ) + SUM( IFNULL( calcuRes, 0 ) ) + SUM( IFNULL( wfs_fulfillment, 0 ) ) + SUM( IFNULL( ddp, 0 ) ) + SUM( IFNULL( adCost, 0 ) ) + SUM( IFNULL( warehouse_rent, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( wfs_return_shipping, 0 ) ) + SUM( IFNULL( adjustment, 0 ) ) + SUM( IFNULL( lc_adjustment, 0 ) ) + SUM( IFNULL( le_adjustment, 0 ) ) + SUM( IFNULL( wfs_adjustment, 0 ) ) + SUM( IFNULL( operation_expenses, 0 ) ) + SUM( IFNULL( operation_factory, 0 ) ) + SUM( IFNULL( operation_delivery, 0 ) ),
 		2 
 	) profit,
 	ROUND(
 		(
-			SUM( IFNULL( sale_amount, 0 ) ) + SUM( IFNULL( refund_amount, 0 ) ) + SUM( IFNULL( sale_selling_fees, 0 ) ) + SUM( IFNULL( refund_selling_fees, 0 ) ) + SUM( IFNULL( calcuRes, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( ddp, 0 ) ) + SUM( IFNULL( adCost, 0 ) ) + SUM( IFNULL( warehouse_rent, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( adjustment, 0 ) ) + SUM( IFNULL( lc_adjustment, 0 ) ) + SUM( IFNULL( le_adjustment, 0 ) ) + SUM( IFNULL( wfs_adjustment, 0 ) ) 
+			SUM( IFNULL( sale_amount, 0 ) ) + SUM( IFNULL( refund_amount, 0 ) ) + SUM( IFNULL( sale_selling_fees, 0 ) ) + SUM( IFNULL( refund_selling_fees, 0 ) ) + SUM( IFNULL( calcuRes, 0 ) ) + SUM( IFNULL( wfs_fulfillment, 0 ) ) + SUM( IFNULL( ddp, 0 ) ) + SUM( IFNULL( adCost, 0 ) ) + SUM( IFNULL( warehouse_rent, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( wfs_return_shipping, 0 ) ) + SUM( IFNULL( adjustment, 0 ) ) + SUM( IFNULL( lc_adjustment, 0 ) ) + SUM( IFNULL( le_adjustment, 0 ) ) + SUM( IFNULL( wfs_adjustment, 0 ) ) + SUM( IFNULL( operation_expenses, 0 ) ) + SUM( IFNULL( operation_factory, 0 ) ) + SUM( IFNULL( operation_delivery, 0 ) ) 
 		) / SUM( IFNULL( sale_amount, 0 ) ),
 		4 
 	) gross_profit_margin,
 	SUM( ROUND( evaluation_qty, 3 ) ) evaluation_qty,
 	SUM( ROUND( evaluation_amount, 2 ) ) evaluation_amount,
 	ROUND(
-		SUM( IFNULL( sale_amount, 0 ) ) + SUM( IFNULL( refund_amount, 0 ) ) + SUM( IFNULL( sale_selling_fees, 0 ) ) + SUM( IFNULL( refund_selling_fees, 0 ) ) + SUM( IFNULL( calcuRes, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( ddp, 0 ) ) + SUM( IFNULL( adCost, 0 ) ) + SUM( IFNULL( warehouse_rent, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( adjustment, 0 ) ) + SUM( IFNULL( lc_adjustment, 0 ) ) + SUM( IFNULL( le_adjustment, 0 ) ) + SUM( IFNULL( wfs_adjustment, 0 ) ) + SUM( IFNULL( evaluation_amount, 0 ) ),
+		SUM( IFNULL( sale_amount, 0 ) ) + SUM( IFNULL( refund_amount, 0 ) ) + SUM( IFNULL( sale_selling_fees, 0 ) ) + SUM( IFNULL( refund_selling_fees, 0 ) ) + SUM( IFNULL( calcuRes, 0 ) ) + SUM( IFNULL( wfs_fulfillment, 0 ) ) + SUM( IFNULL( ddp, 0 ) ) + SUM( IFNULL( adCost, 0 ) ) + SUM( IFNULL( warehouse_rent, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( wfs_return_shipping, 0 ) ) + SUM( IFNULL( adjustment, 0 ) ) + SUM( IFNULL( lc_adjustment, 0 ) ) + SUM( IFNULL( le_adjustment, 0 ) ) + SUM( IFNULL( wfs_adjustment, 0 ) ) + SUM( IFNULL( operation_expenses, 0 ) ) + SUM( IFNULL( operation_factory, 0 ) ) + SUM( IFNULL( operation_delivery, 0 ) ) + SUM( IFNULL( evaluation_amount, 0 ) ),
 		2 
 	) profit_include_evaluation,
 	ROUND(
 		(
-			SUM( IFNULL( sale_amount, 0 ) ) + SUM( IFNULL( refund_amount, 0 ) ) + SUM( IFNULL( sale_selling_fees, 0 ) ) + SUM( IFNULL( refund_selling_fees, 0 ) ) + SUM( IFNULL( calcuRes, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( ddp, 0 ) ) + SUM( IFNULL( adCost, 0 ) ) + SUM( IFNULL( warehouse_rent, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( adjustment, 0 ) ) + SUM( IFNULL( lc_adjustment, 0 ) ) + SUM( IFNULL( le_adjustment, 0 ) ) + SUM( IFNULL( wfs_adjustment, 0 ) ) + SUM( IFNULL( evaluation_amount, 0 ) ) 
+			SUM( IFNULL( sale_amount, 0 ) ) + SUM( IFNULL( refund_amount, 0 ) ) + SUM( IFNULL( sale_selling_fees, 0 ) ) + SUM( IFNULL( refund_selling_fees, 0 ) ) + SUM( IFNULL( calcuRes, 0 ) ) + SUM( IFNULL( wfs_fulfillment, 0 ) ) + SUM( IFNULL( ddp, 0 ) ) + SUM( IFNULL( adCost, 0 ) ) + SUM( IFNULL( warehouse_rent, 0 ) ) + SUM( IFNULL( wfs_warehouse, 0 ) ) + SUM( IFNULL( wfs_return_shipping, 0 ) ) + SUM( IFNULL( adjustment, 0 ) ) + SUM( IFNULL( lc_adjustment, 0 ) ) + SUM( IFNULL( le_adjustment, 0 ) ) + SUM( IFNULL( wfs_adjustment, 0 ) ) + SUM( IFNULL( operation_expenses, 0 ) ) + SUM( IFNULL( operation_factory, 0 ) ) + SUM( IFNULL( operation_delivery, 0 ) ) + SUM( IFNULL( evaluation_amount, 0 ) ) 
 		) / SUM( IFNULL( sale_amount, 0 ) ),
 		2 
 	) gross_profit_margin_include_evaluation,
@@ -2174,15 +2178,19 @@ FROM
 		SUM( ROUND( sale_selling_fees, 7 ) ) * - 1 sale_selling_fees,
 		SUM( ROUND( refund_selling_fees, 7 ) ) refund_selling_fees,
 		SUM( ROUND( calcuRes, 7 ) ) * - 1 calcuRes,
-		SUM( ROUND( wfs_tail, 6 ) ) wfs_tail,
+		SUM( ROUND( wfs_fulfillment, 6 ) ) wfs_fulfillment,
 		SUM( ROUND( ddp, 2 ) ) * - 1 ddp,
 		SUM( ROUND( adCost, 7 ) ) adCost,
 		SUM( ROUND( warehouse_rent, 4 ) ) * - 1 warehouse_rent,
 		SUM( ROUND( wfs_warehouse, 8 ) ) * - 1 wfs_warehouse,
+		SUM( ROUND( wfs_return_shipping, 6 ) ) wfs_return_shipping,
 		SUM( ROUND( adjustment, 6 ) ) adjustment,
 		SUM( ROUND( lc_adjustment, 6 ) ) lc_adjustment,
 		SUM( ROUND( le_adjustment, 6 ) ) le_adjustment,
 		SUM( ROUND( wfs_adjustment, 6 ) ) wfs_adjustment,
+		SUM( ROUND( operation_expenses, 6 ) ) operation_expenses,
+		SUM( ROUND( operation_factory, 6 ) ) operation_factory,
+		SUM( ROUND( operation_delivery, 6 ) ) operation_delivery,
 		SUM( ROUND( evaluation_qty, 3 ) ) evaluation_qty,
 		SUM( ROUND( evaluation_amount, 2 ) ) * - 1 evaluation_amount 
 	FROM
@@ -2198,15 +2206,19 @@ FROM
 			SUM( ROUND( sale_selling_fees, 7 ) ) sale_selling_fees,
 			SUM( ROUND( refund_selling_fees, 7 ) ) refund_selling_fees,
 			SUM( ROUND( calcuRes, 7 ) ) calcuRes,
-			SUM( ROUND( wfs_tail, 6 ) ) wfs_tail,
+			SUM( ROUND( wfs_fulfillment, 6 ) ) wfs_fulfillment,
 			SUM( ROUND( ddp, 2 ) ) ddp,
 			NULL AS adCost,
 			SUM( ROUND( warehouse_rent, 4 ) ) warehouse_rent,
 			SUM( ROUND( wfs_warehouse, 8 ) ) wfs_warehouse,
+			SUM( ROUND( wfs_return_shipping, 6 ) ) wfs_return_shipping,
 			SUM( ROUND( adjustment, 6 ) ) adjustment,
 			SUM( ROUND( lc_adjustment, 6 ) ) lc_adjustment,
 			SUM( ROUND( le_adjustment, 6 ) ) le_adjustment,
 			SUM( ROUND( wfs_adjustment, 6 ) ) wfs_adjustment,
+			NULL AS operation_expenses,
+			NULL AS operation_factory,
+			NULL AS operation_delivery,
 			NULL AS evaluation_qty,
 			NULL AS evaluation_amount 
 		FROM
@@ -2225,11 +2237,12 @@ FROM
 				ROUND( b.selling_fee, 7 ) sale_selling_fees,
 				NULL AS refund_selling_fees,
 				c.calcuRes calcuRes,
-				NULL AS wfs_tail,
+				NULL AS wfs_fulfillment,
 				NULL AS ddp,
 				NULL AS adCost,
 				NULL AS warehouse_rent,
 				NULL AS wfs_warehouse,
+				NULL AS wfs_return_shipping,
 				NULL AS adjustment,
 				NULL AS lc_adjustment,
 				NULL AS le_adjustment,
@@ -2249,7 +2262,7 @@ FROM
 					AND b.platform = "walmart" 
 				) a
 				LEFT JOIN mu_finance_order_statistics b ON a.payment_id = b.payment_id
-				LEFT JOIN mu_ecang_order c ON b.saleOrderCode = c.saleOrderCode
+				LEFT JOIN mu_ecang_order c ON b.saleOrderCode = c.saleOrderCode 
 			WHERE
 				b.payment_id IS NOT NULL UNION ALL
 			SELECT
@@ -2266,11 +2279,12 @@ FROM
 				NULL AS sale_selling_fees,
 				NULL AS refund_selling_fees,
 				NULL AS calcuRes,
-				NULL AS wfs_tail,
+				NULL AS wfs_fulfillment,
 				c.sku_ddp_unit * b.qty / d.USD ddp,
 				NULL AS adCost,
 				NULL AS warehouse_rent,
 				NULL AS wfs_warehouse,
+				NULL AS wfs_return_shipping,
 				NULL AS adjustment,
 				NULL AS lc_adjustment,
 				NULL AS le_adjustment,
@@ -2292,7 +2306,7 @@ FROM
 				LEFT JOIN mu_finance_order_outbound b ON b.payment_id = a.payment_id 
 				AND b.report_id = ' . $report_id . ' 
 				LEFT JOIN mu_finance_store c ON b.store_id = c.id
-				LEFT JOIN mu_finance_report d ON b.report_id = d.id
+				LEFT JOIN mu_finance_report d ON b.report_id = d.id 
 			WHERE
 				b.payment_id IS NOT NULL UNION ALL
 			SELECT
@@ -2309,11 +2323,12 @@ FROM
 				NULL AS sale_selling_fees,
 				ROUND( selling_fees * d.pcr_percent * d.pcr_quantity / 100, 7 ) refund_selling_fees,
 				NULL AS calcuRes,
-				NULL AS wfs_tail,
+				NULL AS wfs_fulfillment,
 				NULL AS ddp,
 				NULL AS adCost,
 				NULL AS warehouse_rent,
 				NULL AS wfs_warehouse,
+				NULL AS wfs_return_shipping,
 				NULL AS adjustment,
 				NULL AS lc_adjustment,
 				NULL AS le_adjustment,
@@ -2341,22 +2356,23 @@ FROM
 				NULL AS sale_selling_fees,
 				NULL AS refund_selling_fees,
 				NULL AS calcuRes,
-				c.total wfs_tail,
+				c.total wfs_fulfillment,
 				NULL AS ddp,
 				NULL AS adCost,
 				NULL AS warehouse_rent,
 				NULL AS wfs_warehouse,
+				NULL AS wfs_return_shipping,
 				NULL AS adjustment,
 				NULL AS lc_adjustment,
 				NULL AS le_adjustment,
 				NULL AS wfs_adjustment 
 			FROM
-				mu_finance_order_adjustment a
+				mu_finance_order_adjustment_wfs a
 				LEFT JOIN mu_finance_table b ON a.table_id = b.id
 				LEFT JOIN mu_finance_order_share c ON a.share_code = c.share_code 
 			WHERE
 				a.report_id = ' . $report_id . ' 
-				AND a.is_wfs_tail = 1 
+				AND a.is_fulfillment = 1 
 				AND b.platform = "walmart" UNION ALL
 			SELECT
 				b.platform,
@@ -2372,11 +2388,44 @@ FROM
 				NULL AS sale_selling_fees,
 				NULL AS refund_selling_fees,
 				NULL AS calcuRes,
-				NULL AS wfs_tail,
+				NULL AS wfs_fulfillment,
 				NULL AS ddp,
 				NULL AS adCost,
 				NULL AS warehouse_rent,
 				NULL AS wfs_warehouse,
+				c.total wfs_return_shipping,
+				NULL AS adjustment,
+				NULL AS lc_adjustment,
+				NULL AS le_adjustment,
+				NULL AS wfs_adjustment 
+			FROM
+				mu_finance_order_adjustment_wfs a
+				LEFT JOIN mu_finance_table b ON a.table_id = b.id
+				LEFT JOIN mu_finance_order_share c ON a.share_code = c.share_code 
+			WHERE
+				a.report_id = ' . $report_id . ' 
+				AND a.is_return_shipping = 1 
+				AND b.platform = "walmart" UNION ALL
+			SELECT
+				b.platform,
+				b.userAccount,
+				a.payment_id,
+				NULL AS saleOrderCode,
+				a.sku seller_sku,
+				c.warehouse_sku warehouse_sku,
+				NULL AS sale_qty,
+				NULL AS refund_qty,
+				NULL AS sale_amount,
+				NULL AS refund_amount,
+				NULL AS sale_selling_fees,
+				NULL AS refund_selling_fees,
+				NULL AS calcuRes,
+				NULL AS wfs_fulfillment,
+				NULL AS ddp,
+				NULL AS adCost,
+				NULL AS warehouse_rent,
+				NULL AS wfs_warehouse,
+				NULL AS wfs_return_shipping,
 				c.total adjustment,
 				NULL AS lc_adjustment,
 				NULL AS le_adjustment,
@@ -2387,7 +2436,6 @@ FROM
 				LEFT JOIN mu_finance_order_share c ON a.share_code = c.share_code 
 			WHERE
 				a.report_id = ' . $report_id . ' 
-				AND a.is_wfs_tail = 0 
 				AND b.platform = "walmart" UNION ALL
 			SELECT
 				"walmart" AS platform,
@@ -2403,11 +2451,12 @@ FROM
 				NULL AS sale_selling_fees,
 				NULL AS refund_selling_fees,
 				NULL AS calcuRes,
-				NULL AS wfs_tail,
+				NULL AS wfs_fulfillment,
 				NULL AS ddp,
 				NULL AS adCost,
 				NULL AS warehouse_rent,
 				NULL AS wfs_warehouse,
+				NULL AS wfs_return_shipping,
 				NULL AS adjustment,
 				b.total lc_adjustment,
 				NULL AS le_adjustment,
@@ -2435,11 +2484,12 @@ FROM
 				NULL AS sale_selling_fees,
 				NULL AS refund_selling_fees,
 				NULL AS calcuRes,
-				NULL AS wfs_tail,
+				NULL AS wfs_fulfillment,
 				NULL AS ddp,
 				NULL AS adCost,
 				NULL AS warehouse_rent,
 				NULL AS wfs_warehouse,
+				NULL AS wfs_return_shipping,
 				NULL AS adjustment,
 				NULL AS lc_adjustment,
 				b.total le_adjustment,
@@ -2467,18 +2517,19 @@ FROM
 				NULL AS sale_selling_fees,
 				NULL AS refund_selling_fees,
 				NULL AS calcuRes,
-				NULL AS wfs_tail,
+				NULL AS wfs_fulfillment,
 				NULL AS ddp,
 				NULL AS adCost,
 				NULL AS warehouse_rent,
 				NULL AS wfs_warehouse,
+				NULL AS wfs_return_shipping,
 				NULL AS adjustment,
 				NULL AS lc_adjustment,
 				NULL AS le_adjustment,
 				wfs_adjustment wfs_adjustment 
 			FROM
 				mu_finance_order_additional a
-				LEFT JOIN ( SELECT DISTINCT platform, userAccount FROM mu_finance_table WHERE rid = ' . $report_id . '  ) b ON a.user_account = b.userAccount 
+				LEFT JOIN ( SELECT DISTINCT platform, userAccount FROM mu_finance_table WHERE rid = ' . $report_id . ' ) b ON a.user_account = b.userAccount 
 			WHERE
 				report_id = ' . $report_id . ' 
 				AND b.platform = "walmart" UNION ALL
@@ -2496,12 +2547,13 @@ FROM
 				NULL AS sale_selling_fees,
 				NULL AS refund_selling_fees,
 				NULL AS calcuRes,
-				NULL AS wfs_tail,
+				NULL AS wfs_fulfillment,
 				NULL AS ddp,
 				NULL AS adCost,
 				NULL AS warehouse_rent,
 				ROUND( a.total * c.pcr_percent / 100, 7 ) wfs_warehouse,
 				NULL AS adjustment,
+				NULL AS wfs_return_shipping,
 				NULL AS lc_adjustment,
 				NULL AS le_adjustment,
 				NULL AS wfs_adjustment 
@@ -2526,12 +2578,13 @@ FROM
 				NULL AS sale_selling_fees,
 				NULL AS refund_selling_fees,
 				NULL AS calcuRes,
-				NULL AS wfs_tail,
+				NULL AS wfs_fulfillment,
 				NULL AS ddp,
 				NULL AS adCost,
 				ROUND( SUM( b.total ), 6 ) warehouse_rent,
 				NULL AS wfs_warehouse,
 				NULL AS adjustment,
+				NULL AS wfs_return_shipping,
 				NULL AS lc_adjustment,
 				NULL AS le_adjustment,
 				NULL AS wfs_adjustment 
@@ -2541,7 +2594,6 @@ FROM
 				LEFT JOIN ( SELECT DISTINCT platform, userAccount FROM mu_finance_table WHERE rid = ' . $report_id . ' ) c ON b.user_account = c.userAccount 
 			WHERE
 				a.report_id = ' . $report_id . ' 
-				AND b.report_id = ' . $report_id . '
 				AND c.platform = "walmart" 
 			GROUP BY
 				platform,
@@ -2563,15 +2615,19 @@ FROM
 			NULL AS sale_selling_fees,
 			NULL AS refund_selling_fees,
 			NULL AS calcuRes,
-			NULL AS wfs_tail,
+			NULL AS wfs_fulfillment,
 			NULL AS ddp,
 			NULL AS adCost,
 			NULL AS warehouse_rent,
 			NULL AS wfs_warehouse,
+			NULL AS wfs_return_shipping,
 			NULL AS adjustment,
 			NULL AS lc_adjustment,
 			NULL AS le_adjustment,
 			NULL AS wfs_adjustment,
+			NULL AS operation_expenses,
+			NULL AS operation_factory,
+			NULL AS operation_delivery,
 			b.qty evaluation_qty,
 			ROUND( a.cny_actual_paid / c.USD, 2 ) evaluation_amount 
 		FROM
@@ -2594,22 +2650,125 @@ FROM
 			NULL AS sale_selling_fees,
 			NULL AS refund_selling_fees,
 			NULL AS calcuRes,
-			NULL AS wfs_tail,
+			NULL AS wfs_fulfillment,
 			NULL AS ddp,
 			total adCost,
 			NULL AS warehouse_rent,
 			NULL AS wfs_warehouse,
+			NULL AS wfs_return_shipping,
 			NULL AS adjustment,
 			NULL AS lc_adjustment,
 			NULL AS le_adjustment,
 			NULL AS wfs_adjustment,
+			NULL AS operation_expenses,
+			NULL AS operation_factory,
+			NULL AS operation_delivery,
 			NULL AS evaluation_qty,
 			NULL AS evaluation_amount 
 		FROM
 			mu_finance_ad_cost 
 		WHERE
 			report_id = ' . $report_id . ' 
-			AND platform = "walmart" 
+			AND platform = "walmart" UNION ALL
+		SELECT
+			c.platform AS platform,
+			c.userAccount userAccount,
+			b.warehouse_sku warehouse_sku,
+			NULL AS sale_qty,
+			NULL AS refund_qty,
+			NULL AS sale_amount,
+			NULL AS refund_amount,
+			NULL AS sale_selling_fees,
+			NULL AS refund_selling_fees,
+			NULL AS calcuRes,
+			NULL AS wfs_fulfillment,
+			NULL AS ddp,
+			NULL AS adCost,
+			NULL AS warehouse_rent,
+			NULL AS wfs_warehouse,
+			NULL AS wfs_return_shipping,
+			NULL AS adjustment,
+			NULL AS lc_adjustment,
+			NULL AS le_adjustment,
+			NULL AS wfs_adjustment,
+			b.total operation_expenses,
+			NULL AS operation_factory,
+			NULL AS operation_delivery,
+			NULL AS evaluation_qty,
+			NULL AS evaluation_amount 
+		FROM
+			mu_finance_operation_expenses a
+			LEFT JOIN mu_finance_order_share b ON a.share_code = b.share_code
+			LEFT JOIN ( SELECT DISTINCT platform, userAccount FROM mu_finance_table WHERE rid = ' . $report_id . ' ) c ON b.user_account = c.userAccount 
+		WHERE
+			a.report_id = ' . $report_id . ' 
+			AND c.platform = "walmart" UNION ALL
+		SELECT
+			c.platform AS platform,
+			c.userAccount userAccount,
+			b.warehouse_sku warehouse_sku,
+			NULL AS sale_qty,
+			NULL AS refund_qty,
+			NULL AS sale_amount,
+			NULL AS refund_amount,
+			NULL AS sale_selling_fees,
+			NULL AS refund_selling_fees,
+			NULL AS calcuRes,
+			NULL AS wfs_fulfillment,
+			NULL AS ddp,
+			NULL AS adCost,
+			NULL AS warehouse_rent,
+			NULL AS wfs_warehouse,
+			NULL AS wfs_return_shipping,
+			NULL AS adjustment,
+			NULL AS lc_adjustment,
+			NULL AS le_adjustment,
+			NULL AS wfs_adjustment,
+			NULL AS operation_expenses,
+			b.total operation_factory,
+			NULL AS operation_delivery,
+			NULL AS evaluation_qty,
+			NULL AS evaluation_amount 
+		FROM
+			mu_finance_operation_factory a
+			LEFT JOIN mu_finance_order_share b ON a.share_code = b.share_code
+			LEFT JOIN ( SELECT DISTINCT platform, userAccount FROM mu_finance_table WHERE rid = ' . $report_id . ' ) c ON b.user_account = c.userAccount 
+		WHERE
+			a.report_id = ' . $report_id . ' 
+			AND c.platform = "walmart" UNION ALL
+		SELECT
+			c.platform AS platform,
+			c.userAccount userAccount,
+			b.warehouse_sku warehouse_sku,
+			NULL AS sale_qty,
+			NULL AS refund_qty,
+			NULL AS sale_amount,
+			NULL AS refund_amount,
+			NULL AS sale_selling_fees,
+			NULL AS refund_selling_fees,
+			NULL AS calcuRes,
+			NULL AS wfs_fulfillment,
+			NULL AS ddp,
+			NULL AS adCost,
+			NULL AS warehouse_rent,
+			NULL AS wfs_warehouse,
+			NULL AS wfs_return_shipping,
+			NULL AS adjustment,
+			NULL AS lc_adjustment,
+			NULL AS le_adjustment,
+			NULL AS wfs_adjustment,
+			NULL AS operation_expenses,
+			NULL AS operation_factory,
+			b.total operation_delivery,
+			NULL AS evaluation_qty,
+			NULL AS evaluation_amount 
+		FROM
+			mu_finance_operation_delivery a
+			LEFT JOIN mu_finance_order_share b ON a.share_code = b.share_code
+			LEFT JOIN ( SELECT DISTINCT platform, userAccount FROM mu_finance_table WHERE rid = ' . $report_id . ' ) c ON b.user_account = c.userAccount 
+		WHERE
+			a.report_id = ' . $report_id . ' 
+			AND c.platform = "walmart" 
 		) a 
 	GROUP BY
 		platform,
