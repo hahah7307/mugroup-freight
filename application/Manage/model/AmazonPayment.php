b9aecaff70be1315de72329c342327376d7590f2
hahah7307 <1084429753@qq.com>
2024-11-06 11:08:23 +0800

    运费计算1-375（财报沃尔玛销售额抓取逻辑调整)

--- application/Manage/model/AmazonPayment.php
+++ application/Manage/model/AmazonPayment.php
@@ -991,7 +991,7 @@ class AmazonPayment extends Model
                     "quantity"                  =>  $item[7],
                     "fulfillment"               =>  "Seller",
                     "postal"                    =>  $item[18],
-                    "product_sales"             =>  sprintf('%.2f', str_replace(',', '', $item[24])),
+                    "product_sales"             =>  sprintf('%.2f', str_replace(',', '', $item[24]) + str_replace(',', '', $item[71])),
                     "selling_fees"              =>  sprintf('%.2f', str_replace(',', '', $item[22])) * -1,
                     "shipping_credits"          =>  0,
                     "gift_wrap_credits"         =>  0,
