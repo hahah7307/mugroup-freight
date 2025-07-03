0aefb9ae82e607984335a059d9dfeec62df13171
hahah7307 <1084429753@qq.com>
2025-07-03 15:05:06 +0800

    BI-503（野莓基础数据页面优化)

--- application/Manage/view/finance_wildberries/index.php
+++ application/Manage/view/finance_wildberries/index.php
@@ -7,7 +7,6 @@
 <!-- 主体内容 -->
 <div class="layui-body" id="LAY_app_body">
     <div class="right">
-        <a href="{:url('report')}" class="layui-btn layui-btn-danger layui-btn-sm fr"><i class="layui-icon">&#xe603;</i>返回上一页</a>
         <div class="title">Wildberries费用列表</div>
 
         <div class="layui-form">
