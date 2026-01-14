3cf26cb3b7df8712c173af54a9015cc5b8fd4316
hahah7307 <1084429753@qq.com>
2026-01-14 14:24:48 +0800

    BI-577（财报tk账单逻辑更新)

--- application/Manage/model/AmazonPayment.php
+++ application/Manage/model/AmazonPayment.php
@@ -1728,7 +1728,8 @@ class AmazonPayment extends Model
                         + round(str_replace(',', '', $item[34]), 2)
                         + round(str_replace(',', '', $item[36]), 2)
                         + round(str_replace(',', '', $item[41]), 2)
-                        + round(str_replace(',', '', $item[44]), 2),
+                        + round(str_replace(',', '', $item[44]), 2)
+                        + round(str_replace(',', '', $item[45]), 2),
                     "shipping_credits"          =>  0,
                     "gift_wrap_credits"         =>  0,
                     "regulatory_fee"            =>  0,
@@ -1756,7 +1757,7 @@ class AmazonPayment extends Model
                 ];
             }
 
-            if ($item[3] == "Platform reimbursement") {
+            if ($item[3] != "Order") {
                 $this->orderAdjustmentNew[] = [
                     "report_id"                 =>  $reportId,
                     "table_id"                  =>  $tableId,
