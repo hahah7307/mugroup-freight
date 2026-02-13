91a06aba9157230e7c3d31c497bef9f34887a6bf
hahah7307 <1084429753@qq.com>
2026-02-13 10:46:13 +0800

    BI-584（财报wayfair订单抓取优化)

--- application/Manage/model/AmazonPayment.php
+++ application/Manage/model/AmazonPayment.php
@@ -1125,7 +1125,7 @@ class AmazonPayment extends Model
                 $this->userAccount = $orderRefund['userAccount'];
             }
 
-            if (gettype($item[5]) == 'string' && $item[5] == 'PENDING PAYMENT' && !strpos($item['0'], '_CM')) {
+            if (gettype($item[5]) == 'string' && ($item[5] == 'PENDING PAYMENT' || $item[5] == 'PAID') && !strpos($item['0'], '_CM')) {
                 $this->orderSaleNew[] = [
                     "report_id"                 =>  $reportId,
                     "table_id"                  =>  $tableId,
