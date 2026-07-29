cb5a5263ded91c27f9aa4bce7a5f2134d41403dc
hahah7307 <1084429753@qq.com>
2026-07-29 14:37:01 +0800

    BI-650（海外仓仓储费日销预估-2)

--- application/Manage/controller/WarehouseRentController.php
+++ application/Manage/controller/WarehouseRentController.php
@@ -5,7 +5,6 @@ use app\Manage\model\ProductModel;
 use app\Manage\model\StorageWarehouseRentModel;
 use app\Manage\model\WarehouseRentEstimateBatchModel;
 use app\Manage\model\WarehouseRentEstimateModel;
-use app\Manage\model\WarehouseTailModel;
 use DateTime;
 use Exception;
 use PHPExcel_IOFactory;
@@ -15,6 +14,7 @@ use think\db\exception\DataNotFoundException;
 use think\db\exception\ModelNotFoundException;
 use think\exception\DbException;
 use think\Config;
+use think\Session;
 
 class WarehouseRentController extends BaseController
 {
@@ -26,7 +26,7 @@ class WarehouseRentController extends BaseController
         $keyword = $this->request->get('keyword', '', 'htmlspecialchars');
         $this->assign('keyword', $keyword);
         if ($keyword) {
-            $where['saleOrderCode|shipmentNo|refNo|sku'] = ['like', '%' . $keyword . '%'];
+            $where['title'] = ['like', '%' . $keyword . '%'];
         } else {
             $where = [];
         }
@@ -38,125 +38,69 @@ class WarehouseRentController extends BaseController
         $list = $model->where($where)->order('id asc')->paginate($page_num, false, ['query' => ['keyword' => $keyword]]);
         $this->assign('list', $list);
 
-        $this->assign('totalSum', $model->where($where)->sum('total'));
-
+        Session::set(Config::get('BACK_URL'), $this->request->url(), 'manage');
         return view();
     }
 
     /**
-     * @throws DbException
      * @throws PHPExcel_Reader_Exception
-     * @throws \think\Exception
      */
-    public function import(): \think\response\View
+    public function estimate_import()
     {
         // phpexcel
         require_once './static/classes/PHPExcel/Classes/PHPExcel.php';
-
         $filename = input('filename');
-        $month = input('month');
-        $file= "./upload/excel/" . $filename;
+        $origin = input('origin');
+        $file = "./upload/excel/" . $filename;
         $excelReader = PHPExcel_IOFactory::createReaderForFile($file);
         $excelObj = $excelReader->load($file);
-        $worksheets = $excelObj->getSheetNames();
-        $warehouse = 'LC';
-        $worksheet = [];
-        foreach ($worksheets as $k => $v) {
-            if ($v == '运费') {
-                $warehouse = 'LE';
-                $worksheet = $excelObj->getSheet($k);
-//            } elseif (strpos($v, 'B2C')) {
-            } elseif ($v =='B2C') {
-                $worksheet = $excelObj->getSheet($k);
-            }
-        }
+        $worksheet = $excelObj->getSheet(0);
         $data = $worksheet->toArray();
         unset($data[0]);
 
         Db::startTrans();
         try {
-            $warehouseTailData = [];
-            $financeWarehouseTailObj = new WarehouseTailModel();
-            if ($warehouse == "LE") {
+            $tableObj = new WarehouseRentEstimateModel();
+            $table = [
+                'title'         =>  $origin,
+                'created_time'  =>  date('Y-m-d H:i:s')
+            ];
+            if ($id = $tableObj->insertGetId($table)) {
+                $batchData = [];
                 foreach ($data as $item) {
-                    $warehouseTailData[] = [
-                        "saleOrderCode"				=>	$item[1],
-                        "shipmentNo"				=>	$item[4],
-                        "refNo"						=>	$item[5],
-                        "warehouseCode"				=>	$item[10],
-                        "status"					=>	$item[11],
-                        "postalCode"				=>	$item[22],
-                        "shippingDate"				=>	$item[23],
-                        "zone"						=>	intval(substr($item[24], 4, 1)),
-                        "charge_weight"				=>	$item[25],
-                        "real_weight"				=>	$item[26],
-                        "fuel_rate"					=>	$item[28],
-                        "outbound"					=>	$item[29],
-                        "base"						=>	$item[30],
-                        "residential"				=>	$item[31],
-                        "das_1"						=>	$item[34],
-                        "das_2"						=>	$item[35],
-                        "das_3"						=>	$item[36],
-                        "signature"					=>	$item[35],
-                        "ahs"						=>	$item[37],
-                        "oversize"					=>	$item[39],
-                        "ahs_pss"					=>	$item[38],
-                        "oversize_pss"				=>	$item[40],
-                        "fuel_cost"					=>	$item[47],
-                        "total"						=>	$item[52],
-                        "sku"						=>	$item[53],
-                        "length"					=>	$item[54],
-                        "width"						=>	$item[55],
-                        "height"					=>	$item[56],
-                        "month"                     =>  date('Ym', strtotime($month . "-01")),
-                        "warehouseName"             =>  $warehouse
+                    $batchData[] = [
+                        "estimate_id"           =>  $id,
+                        "warehouse_sku"         =>  $item[0],
+                        "warehouse_code"        =>  $item[1],
+                        "store"                 =>  $item[2],
+                        "age"                   =>  $item[3],
+                        "daily_sale"            =>  $item[4],
+                        "is_finished"           =>  0,
                     ];
                 }
-            } elseif ($warehouse == "LC") {
-                foreach ($data as $item) {
-                    $warehouseTailData[] = [
-                        "saleOrderCode"				=>	$item[2],
-                        "shipmentNo"				=>	$item[6],
-                        "refNo"						=>	$item[4],
-                        "warehouseCode"				=>	$item[1],
-                        "postalCode"				=>	$item[12],
-                        "shippingDate"				=>	$item[9],
-                        "zone"						=>	intval($item[15]),
-                        "charge_weight"				=>	$item[13],
-                        "real_weight"				=>	$item[70],
-                        "outbound"					=>	$item[23],
-                        "base"						=>	$item[22],
-                        "residential"				=>	$item[43],
-                        "das_1"						=>	$item[35],
-                        "das_2"						=>	$item[36],
-                        "signature"					=>	$item[33],
-                        "ahs"						=>	$item[26] + $item[27],
-                        "oversize"					=>	$item[28],
-                        "fuel_cost"					=>	$item[24],
-                        "total"						=>	$item[53],
-                        "sku"						=>	substr($item[63], 5),
-                        "length"					=>	$item[65],
-                        "width"						=>	$item[66],
-                        "height"					=>	$item[67],
-                        "month"                     =>  date('Ym', strtotime($month . "-01")),
-                        "warehouseName"             =>  $warehouse
-                    ];
+                $batchObj = new WarehouseRentEstimateBatchModel();
+                if (!$batchObj->insertAll($batchData)) {
+
+                    throw new Exception('表格导入失败');
                 }
+            } else {
+
+                throw new Exception('表格导入失败');
             }
-            $financeWarehouseTailObj->insertAll($warehouseTailData);
 
             Db::commit();
         } catch (Exception $e) {
             Db::rollback();
-            $this->error($e->getMessage(), url('index'));
+            $this->error($e->getMessage(), session('back_url', '', 'manage'));
         }
-        $this->redirect(url('index'));
+        $this->redirect(session('back_url', '', 'manage'));
     }
 
     /**
      * @throws DataNotFoundException
      * @throws DbException
      * @throws ModelNotFoundException
+     * @throws Exception
      */
     public function calculation()
     {
@@ -185,6 +129,7 @@ class WarehouseRentController extends BaseController
 
         $estimateBatch = $estimateBatchObj->where(['estimate_id' => $estimateBatchItem['estimate_id'], 'warehouse_sku' =>$estimateBatchItem['warehouse_sku'], 'is_finished' => 0])->order('age desc')->select();
         $day = 0;
+        $restStore = 0;
         $updateData =[];
         foreach ($estimateBatch as $batch) {
             $storageSum = 0;
@@ -194,14 +139,18 @@ class WarehouseRentController extends BaseController
             }
             unset($i);
             unset($unit);
+            $useDay = ceil($batch['store'] / $batch['daily_sale']);
+            $sum = 0;
+            $batch['store'] -= $restStore;
             if (empty($estimate['end_date'])) {
-
-                $useDay = ceil($batch['store'] / $batch['daily_sale'], 2);
-                $sum = 0;
                 for ($i = 0; $i < $useDay; $i ++) {
                     $store = $batch['store'] - $i * $batch['daily_sale'];
-                    $unit = self::warehouseUnit($batch['age'] + $day + $i, $batch['warehouse_code']);
-                    $sum += $store * $unit;
+                    if ($store >= 0) {
+                        $unit = self::warehouseUnit($batch['age'] + $day + $i, $batch['warehouse_code']);
+                        $sum += $store * $unit;
+                    } else {
+                        $useDay -= 1;
+                    }
                 }
                 $day += $useDay;
                 $updateData[] = [
@@ -211,14 +160,15 @@ class WarehouseRentController extends BaseController
                     'is_finished'   =>  1
                 ];
             } else {
-
-                $useDay = ceil($batch['store'] / $batch['daily_sale']);
-                $sum = 0;
                 if ($useDay >= $days) {
                     for ($i = 0; $i < $days; $i ++) {
                         $store = $batch['store'] - $i * $batch['daily_sale'];
-                        $unit = self::warehouseUnit($batch['age']+ $day + $i, $batch['warehouse_code']);
-                        $sum += $store * $unit;
+                        if ($store >= 0) {
+                            $unit = self::warehouseUnit($batch['age']+ $day + $i, $batch['warehouse_code']);
+                            $sum += $store * $unit;
+                        } {
+                            $useDay -= 1;
+                        }
                     }
                     $day += $days;
                     $updateData[] = [
@@ -231,8 +181,12 @@ class WarehouseRentController extends BaseController
                 } else {
                     for ($i = 0; $i < $useDay; $i ++) {
                         $store = $batch['store'] - $i * $batch['daily_sale'];
-                        $unit = self::warehouseUnit($batch['age'] + $day + $i, $batch['warehouse_code']);
-                        $sum += $store * $unit;
+                        if ($store >= 0) {
+                            $unit = self::warehouseUnit($batch['age'] + $day + $i, $batch['warehouse_code']);
+                            $sum += $store * $unit;
+                        } {
+                            $useDay -= 1;
+                        }
                     }
                     $day += $useDay;
                     $updateData[] = [
@@ -244,6 +198,7 @@ class WarehouseRentController extends BaseController
                     $days -= $useDay;
                 }
             }
+            $restStore = round($useDay * $batch['daily_sale'] - $batch['store'], 2);
             unset($storageSum);
         }
         dump($estimateBatchObj->saveAll($updateData));
---
+++ application/Manage/view/warehouse_rent/estimate.php
@@ -0,0 +1,114 @@
+
+{include file="public/header" /}
+
+<!-- 主体内容 -->
+<div class="layui-body" id="LAY_app_body">
+    <div class="right">
+        <a href="{:url('Storage/index')}" class="layui-btn layui-btn-danger layui-btn-sm fr"><i class="layui-icon">&#xe603;</i>返回上一页</a>
+        <div class="title">出入库费用列表</div>
+
+        <div class="layui-form">
+            <button type="button" class="layui-btn  layui-btn-normal" id="excel">导入</button>
+            <table class="layui-table" lay-size="sm">
+                <colgroup>
+                    <col class="w80">
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                    <col width="180">
+                </colgroup>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>表格标题</th>
+                    <th>开始日期</th>
+                    <th>结束日期</th>
+                    <th>创建时间</th>
+                    <th class="tc">操作</th>
+                </tr>
+                </thead>
+                <tbody>
+                {foreach name="list" item="v"}
+                <tr>
+                    <td class="tr">{$v.id}</td>
+                    <td>{$v.title}</td>
+                    <td class="tr">{$v.start_date}</td>
+                    <td class="tr">{$v.end_date}</td>
+                    <td>{$v.created_time}</td>
+                    <td class="tc">
+                        <a href="{:url('edit', ['id' => $v.id])}" class="layui-btn layui-btn-normal layui-btn-sm">编辑</a>
+                        <button data-id="{$v.id}" class="layui-btn layui-btn-sm layui-btn-danger ml0" lay-submit lay-filter="Detele">删除</button>
+                    </td>
+                </tr>
+                {/foreach}
+                </tbody>
+            </table>
+            {$list->render()}
+        </div>
+
+    </div>
+</div>
+<script>
+    layui.use(['form', 'upload', 'jquery'], function(){
+        let $ = layui.jquery,
+            upload = layui.upload,
+            form = layui.form;
+
+        // 上传
+        let uploadInst = upload.render({
+            elem: '#excel' //绑定元素
+            ,url: '/Manage/upload/file_upload' //上传接口
+            ,exts: 'xls|xlsx|csv'
+            ,multiple: true
+            ,before: function (obj){
+                layer.load(1);
+            }
+            ,done: function(res){
+                //上传完毕回调
+                if (res.code === 1) {
+                    location.href = "/Manage/WarehouseRent/estimate_import/filename/" + res.data + "/origin/" + res.origin;
+                } else {
+                    layer.alert(res.msg,{icon:2,closeBtn:0,title:false,btnAlign:'c'},function(){
+                        layer.closeAll();
+                    });
+                }
+            }
+            ,error: function(){
+                //请求异常回调
+            }
+        });
+
+        // 删除
+        form.on('submit(Detele)', function(data){
+            var text = $(this).text(),
+                button = $(this),
+                id = $(this).data('id');
+            layer.confirm('确定删除吗？',{icon:3,closeBtn:0,title:false,btnAlign:'c'},function(){
+                $('button').attr('disabled',true);
+                button.text('请稍候...');
+                axios.post("{:url('delete')}", {id:id})
+                    .then(function (response) {
+                        var res = response.data;
+                        if (res.code === 1) {
+                            layer.alert(res.msg,{icon:1,closeBtn:0,title:false,btnAlign:'c',},function(){
+                                location.reload();
+                            });
+                        } else {
+                            layer.alert(res.msg,{icon:2,closeBtn:0,title:false,btnAlign:'c'},function(){
+                                layer.closeAll();
+                                $('button').attr('disabled',false);
+                                button.text(text);
+                            });
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                return false;
+            });
+        });
+    });
+</script>
+
+{include file="public/footer" /}
